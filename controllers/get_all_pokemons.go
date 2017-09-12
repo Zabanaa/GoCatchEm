bf3fd24c48922726d075a4e85dc6ca6e3efa6209
Karim Cheurfi <karim.cheurfi@gmail.com>
2017-09-12 20:55:24 +0000

    move the main logic inside the controllers

--- controllers/get_all_pokemons.go
+++ controllers/get_all_pokemons.go
@@ -8,13 +8,51 @@ import (
 
 func GetAllPokemons(db *sql.DB, w http.ResponseWriter, r *http.Request) {
 
-	pokemons, err := models.GetPokemons(db)
+	var response Response
+
+	rows, err := db.Query("SELECT * FROM pokemons")
 
 	if err != nil {
-		// repond with error
-		respondWithError(w, http.StatusInternalServerError, err.Error())
+
+		response.Meta.Type = "internal server error"
+		response.Meta.StatusCode = http.StatusInternalServerError
+		response.Body = map[string]interface{}{"error": err.Error()}
+
+		respondWithJson(w, http.StatusInternalServerError, response)
 	}
 
+	defer rows.Close()
+
+	var pokemons []models.Pokemon
+
+	for rows.Next() {
+
+		var pokemon models.Pokemon
+
+		err := rows.Scan(
+			&pokemon.ID, &pokemon.Number, &pokemon.Name, &pokemon.JpName,
+			&pokemon.Types, &pokemon.Stats.HP, &pokemon.Stats.Attack,
+			&pokemon.Stats.Defense, &pokemon.Stats.Sp_atk, &pokemon.Stats.Sp_def,
+			&pokemon.Stats.Speed, &pokemon.Bio, &pokemon.Generation)
+
+		if err != nil {
+
+			response.Meta.Type = "internal server error"
+			response.Meta.StatusCode = http.StatusInternalServerError
+			response.Body = map[string]interface{}{"error": err.Error()}
+
+			respondWithJson(w, http.StatusInternalServerError, response)
+		}
+
+		pokemons = append(pokemons, pokemon)
+	}
+
+	response.Meta.Type = "success"
+	response.Meta.StatusCode = http.StatusOK
+	response.Body = make(map[string]interface{})
+	response.Body["count"] = len(pokemons)
+	response.Body["data"] = pokemons
+
 	// Respond with the pokemons
-	respondWithJson(w, http.StatusOK, pokemons)
+	respondWithJson(w, http.StatusOK, response)
 }
--- controllers/get_one_pokemon.go
+++ controllers/get_one_pokemon.go
@@ -1,10 +1,41 @@
 package controllers
 
 import (
-	"fmt"
+	"database/sql"
 	"net/http"
+	"pokemon_api/models"
+
+	"github.com/gorilla/mux"
 )
 
-func getPokemon(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "hello world")
+func GetPokemon(db *sql.DB, w http.ResponseWriter, req *http.Request) {
+
+	var response Response
+
+	vars := mux.Vars(req)
+	pokemonName := vars["name"]
+
+	var pokemon models.Pokemon
+
+	err := db.QueryRow("SELECT * FROM pokemons WHERE name=$1", pokemonName).Scan(
+		&pokemon.ID, &pokemon.Number, &pokemon.Name, &pokemon.JpName,
+		&pokemon.Types, &pokemon.Stats.HP, &pokemon.Stats.Attack,
+		&pokemon.Stats.Defense, &pokemon.Stats.Sp_atk, &pokemon.Stats.Sp_def,
+		&pokemon.Stats.Speed, &pokemon.Bio, &pokemon.Generation)
+
+	if err != nil {
+
+		if err == sql.ErrNoRows {
+			// 404 not found
+			// respondWithError(w, http.StatusNotFound, "Pokemon not found")
+			return
+		} else {
+			// 500 Internal Server Error
+			// respondWithError(w, http.StatusInternalServerError, err.Error())
+			return
+		}
+	}
+	response.Body = make(map[string]interface{})
+	response.Body["pokemon"] = pokemon
+	respondWithJson(w, http.StatusOK, response)
 }
