60f5e2ec0adfb4e6ddaff7f0a8774570cc69ca1e
Karim Cheurfi <karim.cheurfi@gmail.com>
2017-09-12 20:54:47 +0000

    remove respondWithError function and create Response struct

--- controllers/helpers.go
+++ controllers/helpers.go
@@ -5,15 +5,20 @@ import (
 	"net/http"
 )
 
-func respondWithError(w http.ResponseWriter, code int, message string) {
+type Response struct {
+	Meta struct {
+		Type       string `json:"type, omitempty"`
+		StatusCode int64  `json:"status_code, omitempty"`
+	}
 
-	respondWithJson(w, code, map[string]string{"error": message})
+	Body map[string]interface{} `json:"body, omitempty"`
 }
 
-func respondWithJson(w http.ResponseWriter, code int, payload interface{}) {
+func respondWithJson(w http.ResponseWriter, code int, payload Response) {
 
 	response, _ := json.Marshal(payload)
 	w.Header().Set("content-type", "application/json")
 	w.WriteHeader(code)
 	w.Write(response)
+
 }
