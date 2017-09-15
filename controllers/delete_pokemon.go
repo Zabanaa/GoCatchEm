18f9a26da2dc6c27f93324c70783eb7eb28a1115
Karim Cheurfi <karim.cheurfi@gmail.com>
2017-09-15 13:41:46 +0000

    create delete pokemon controllers

--- controllers/delete_pokemon.go
+++ controllers/delete_pokemon.go
@@ -3,13 +3,41 @@ package controllers
 import (
 	"database/sql"
 	"fmt"
+	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
-func deletePokemon(db *sql.DB, w http.ResponseWriter, r *http.Request) {
+func DeletePokemon(db *sql.DB, w http.ResponseWriter, r *http.Request) {
 
-	// Fetch a pokemon by ID
-	// Delete the motherfucker
-	// return a 204
-	fmt.Fprintf(w, "hello world")
+	vars := mux.Vars(r)
+	pokemonName := vars["name"]
+	var response Response
+
+	query := `DELETE FROM pokemons WHERE id=$1`
+	result, err := db.Exec(query, pokemonName)
+
+	if err != nil {
+
+		if err == sql.ErrNoRows {
+			response.NotFound(w, "Pokemon not found.")
+			return
+		} else {
+			response.ServerError(w, err.Error())
+			return
+		}
+	}
+
+	count, err := result.RowsAffected()
+
+	if err != nil {
+		// return an error ?
+		fmt.Println(err.Error())
+	}
+
+	log.Println("Deleted", count, "rows")
+
+	response.Deleted(w)
+
 }
