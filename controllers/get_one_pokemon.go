fa75a503b4cfaf4715281f5408691b12e0e79cb1
Karim Cheurfi <karim.cheurfi@gmail.com>
2017-09-15 13:43:02 +0000

    extract query to its own variable

--- controllers/get_one_pokemon.go
+++ controllers/get_one_pokemon.go
@@ -13,12 +13,13 @@ func GetPokemon(db *sql.DB, w http.ResponseWriter, req *http.Request) {
 	var response Response
 	var errorMessage string
 
+	query := "SELECT * FROM pokemons WHERE name=$1 ORDER BY id"
 	vars := mux.Vars(req)
 	pokemonName := vars["name"]
 
 	var pokemon models.Pokemon
 
-	err := db.QueryRow("SELECT * FROM pokemons WHERE name=$1 ORDER BY id", pokemonName).Scan(
+	err := db.QueryRow(query, pokemonName).Scan(
 		&pokemon.ID, &pokemon.Number, &pokemon.Name, &pokemon.JpName,
 		&pokemon.Types, &pokemon.Stats.HP, &pokemon.Stats.Attack,
 		&pokemon.Stats.Defense, &pokemon.Stats.Sp_atk, &pokemon.Stats.Sp_def,
