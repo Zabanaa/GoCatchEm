54e7af3c2a2dd83f8bb63c62c57c50640cdc464c
Karim Cheurfi <karim.cheurfi@gmail.com>
2017-09-17 10:04:15 +0000

    create MissingFields and Conflict response helpers along with extractMissingFields helper

--- controllers/helpers.go
+++ controllers/helpers.go
@@ -3,6 +3,8 @@ package controllers
 import (
 	"encoding/json"
 	"net/http"
+	"regexp"
+	"strings"
 )
 
 type Response struct {
@@ -20,6 +22,30 @@ func (response *Response) StatusOK(w http.ResponseWriter, body map[string]interf
 	response.Meta.Type = "success"
 	response.Meta.StatusCode = http.StatusOK
 	response.Body = body
+	respondWithJson(w, response.Meta.StatusCode, response)
+}
+
+func (response *Response) MissingFields(w http.ResponseWriter, missingField string) {
+
+	response.Meta.Type = "error"
+	response.Meta.StatusCode = http.StatusUnprocessableEntity
+	response.Body = map[string]interface{}{
+		"message":        "Missing fields",
+		"missing_fields": missingField,
+	}
+	respondWithJson(w, response.Meta.StatusCode, response)
+}
+
+// This Helper should really accept a field name
+// To handle the case where I add another unique constraint to the pokemon table
+// but it will do for now
+func (response *Response) Conflict(w http.ResponseWriter) {
+
+	response.Meta.Type = "error"
+	response.Meta.StatusCode = http.StatusConflict
+	response.Body = map[string]interface{}{
+		"message": "A pokemon with this name already exists",
+	}
 	respondWithJson(w, response.Meta.StatusCode, response)
 }
 
@@ -76,4 +102,10 @@ func respondWithJson(w http.ResponseWriter, code int64, payload *Response) {
 	w.Header().Set("content-type", "application/json")
 	w.WriteHeader(int(code)) // We convert code to an int because w.WriteHeader does not accept Int64 type values
 	w.Write(response)
+}
+
+func extractMissingField(errorMessage string) string {
+	pattern := regexp.MustCompile(`\"(\w+)\"`)
+	missingField := strings.Replace(pattern.FindString(errorMessage), "\"", "", 2)
+	return missingField
 }
