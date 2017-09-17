9bc935b0963574b7f9d587a51b08bb4242a0762c
Karim Cheurfi <karim.cheurfi@gmail.com>
2017-09-17 10:01:26 +0000

    change struct to fields to pointers to handle null values

--- models/Pokemon.go
+++ models/Pokemon.go
@@ -1,21 +1,21 @@
 package models
 
 type Pokemon struct {
-	ID         int    `json:"id, omitempty"`
-	Number     string `json:"number"`
-	Name       string `json:"name"`
-	JpName     string `json:"jp_name"`
-	Types      string `json:"types"`
-	Stats      Stats  `json:"stats"`
-	Bio        string `json:"bio"`
-	Generation int64  `json:"generation"`
+	ID         int     `json:"id, omitempty"`
+	Number     *string `json:"number"`
+	Name       *string `json:"name"`
+	JpName     *string `json:"jp_name"`
+	Types      *string `json:"types"`
+	Stats      Stats   `json:"stats"`
+	Bio        *string `json:"bio"`
+	Generation *int64  `json:"generation"`
 }
 
 type Stats struct {
-	HP      int `json:"hp"`
-	Attack  int `json:"attack"`
-	Defense int `json:"defense"`
-	Sp_atk  int `json:"sp_atk"`
-	Sp_def  int `json:"sp_def"`
-	Speed   int `json:"speed"`
+	HP      *int `json:"hp"`
+	Attack  *int `json:"attack"`
+	Defense *int `json:"defense"`
+	Sp_atk  *int `json:"sp_atk"`
+	Sp_def  *int `json:"sp_def"`
+	Speed   *int `json:"speed"`
 }
